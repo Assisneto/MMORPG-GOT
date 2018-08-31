5b4923ee893ea6ad7bbc54ece1a915d044547065
Assisneto <netodamasceno12@gmail.com>
2018-08-30 23:42:30 -0300

    validação na tela de cadastro

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,21 @@
-module.exports.cadastro = (application, re, res)=>{
+module.exports.cadastro = (application, req, res)=>{
   res.render('cadastro');
+}
+
+module.exports.cadastrar = (application, req, res)=>{
+  
+  let dadosForm = req.body  
+
+  req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+  req.assert('usuario','Usuário não pode ser vazio').notEmpty();
+  req.assert('senha','Senha não pode ser vazio').notEmpty();
+  req.assert('casa','Casa não pode ser vazio');
+
+  let err = req.validationErrors();
+
+  if(err){
+    res.send('existem erros no formulario');
+    return;
+  }
+  res.send("podemos cadastrar");
 }
--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -2,4 +2,7 @@ module.exports = (application)=>{
 	application.get('/cadastro',(req, res)=>{
 		application.app.controllers.cadastro.cadastro(application,req,res);
 	});
+	application.post('/cadastrar',(req, res)=>{
+		application.app.controllers.cadastro.cadastrar(application,req,res);
+	});
 }
